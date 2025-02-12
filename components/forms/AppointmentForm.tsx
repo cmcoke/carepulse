6d4d4fcaca9824f2248e026bc03843f530a05ee3
cmcoke <minoru.cm@gmail.com>
2025-02-12 12:25:49 -0500

    created the admin dashboard and implemented SMS notifications using appwrite and twilio

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -13,9 +13,10 @@ import { FormFieldType } from "./PatientForm";
 import Image from "next/image";
 import { Doctors } from "@/constants";
 import { SelectItem } from "../ui/select";
-import { createAppointment } from "@/lib/actions/appointment.actions";
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions";
+import { Appointment } from "@/types/appwrite.types";
 
-const AppointmentForm = ({ userId, patientId, type = "create" }: { userId: string; patientId: string; type: "create" | "schedule" | "cancel" }) => {
+const AppointmentForm = ({ userId, patientId, type = "create", appointment, setOpen }: { userId: string; patientId: string; type: "create" | "schedule" | "cancel"; appointment?: Appointment; setOpen: (open: boolean) => void }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
@@ -25,10 +26,11 @@ const AppointmentForm = ({ userId, patientId, type = "create" }: { userId: strin
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-      primaryPhysician: "",
-      schedule: new Date(),
-      reason: "",
-      cancellationReason: ""
+      primaryPhysician: appointment ? appointment?.primaryPhysician : "",
+      schedule: appointment ? new Date(appointment?.schedule!) : new Date(Date.now()),
+      reason: appointment ? appointment.reason : "",
+      note: appointment?.note || "",
+      cancellationReason: appointment?.cancellationReason || ""
     }
   });
 
@@ -66,6 +68,25 @@ const AppointmentForm = ({ userId, patientId, type = "create" }: { userId: strin
           form.reset();
           router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`);
         }
+      } else {
+        const appointmentToUpdate = {
+          userId,
+          appointmentId: appointment?.$id!,
+          appointment: {
+            primaryPhysician: values.primaryPhysician,
+            schedule: new Date(values.schedule),
+            status: status as Status,
+            cancellationReason: values.cancellationReason
+          },
+          type
+        };
+
+        const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+        if (updatedAppointment) {
+          setOpen && setOpen(false);
+          form.reset();
+        }
       }
     } catch (error) {
       console.log(error);
@@ -88,10 +109,12 @@ const AppointmentForm = ({ userId, patientId, type = "create" }: { userId: strin
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
-        <section className="mb-12 space-y-4">
-          <h1 className="header">New Appointment</h1>
-          <p className="text-dark-700"> Request a new appointment in 10 seconds.</p>
-        </section>
+        {type === "create" && (
+          <section className="mb-12 space-y-4">
+            <h1 className="header">New Appointment</h1>
+            <p className="text-dark-700"> Request a new appointment in 10 seconds.</p>
+          </section>
+        )}
 
         {type !== "cancel" && (
           <>
